c821d040bd1133b56ed0312d9a2330ed6eb017ec
Vishesh Sharma <sharmavishesh308@gmail.com>
2024-12-05 19:10:44 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+
+    private static final String STUDENT_FILE = "students.txt";
+    private static final String ATTENDANCE_FILE = "attendance.csv";
+
+    public static void main(String[] args) {
+        new AttendanceManagementSystem().run();
+    }
